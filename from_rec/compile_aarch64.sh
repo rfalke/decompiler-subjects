9a987fe29b31229c01fac82166b82dfcfd690fa0
Raimar Falke <i-git-stone@rf.risimo.net>
2018-07-10 06:05:50 +0200

    Add arm64 (aka aarch64) compilations of rec subjects

---
+++ from_rec/compile_aarch64.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+#set -x
+
+find . -name source.c | sort | while read line
+do
+	echo "=== $line"
+	for opt in O0 O1 O2 O3 Os
+	do
+        for cc in gcc clang
+        do
+        	echo " == $opt $cc"
+        	bdir=$(dirname $line)
+			edir="$bdir/arm64_elf_${cc}_${opt}_from_rfalke"
+			mkdir -p "$edir"
+			${cc} -w -$opt -g $line -o "$edir/subject.exe"
+			chmod a-x "$edir/subject.exe"
+        done
+	done
+done
