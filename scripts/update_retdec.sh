97b1f6f137e563f85fbcf2a18dfd8de8d5358359
Raimar Falke <i-git-stone@rf.risimo.net>
2018-09-10 17:14:14 +0200

    Partial run with self compiled retdec (git hash d57764a)

--- scripts/update_retdec.sh
+++ scripts/update_retdec.sh
@@ -4,15 +4,16 @@ set -e
 
 ulimit -c 0
 ulimit -t 600
+ulimit -v 4000000
 
-if test -z "$RETDECAPIKEY"; then
-    echo "\$RETDECAPIKEY not set. The API key from \$RETDECAPIKEY will be used."
+if test -z "$RETDECDIR"; then
+    echo "\$RETDECDIR not set. The file \$RETDECDIR/bin/retdec-decompiler.py will be used."
     exit 1
 fi
 
 root=$(pwd)
 
-find $root -name subject.exe | grep -v amiga | grep -v sparc | grep -v _mz_ | grep -v x64 | grep -v sharedexec | grep -v from_pouet.net | grep -v malware_via_email | sort | while read line
+find $root -name subject.exe | grep -v amiga | grep -v sparc | grep -v _mz_ | grep -v x64 | grep -v arm64 | grep -v sharedexec | grep -v from_pouet.net | grep -v malware_via_email | grep holdec | grep dmi | sort | while read line
 do
   dir=$(dirname $line)
   cfile=$(echo $line | sed 's,/subject.exe,/subject.c,')
@@ -21,19 +22,14 @@ do
   else
       echo -n "decompiling $line"
       rm -f $dir/by_retdec.c $dir/by_retdec.failed
-      if $root/scripts/update_retdec_helper.sh -k "$RETDECAPIKEY" "$line" >out 2>&1 ; then
-	  echo "  ok"
-	  sed <$cfile -e "s,// Decompilation date:.*,// Decompilation date: DATE," -e "s,// Decompiler release: .*,// Decompiler release: VERSION,">$dir/by_retdec.c
+      if $RETDECDIR/bin/retdec-decompiler.py --no-config --cleanup --no-config --no-memory-limit -o "$cfile" "$line" >out 2>&1 ; then
+        echo "  ok"
+        sed <$cfile -e "s,$root/,,g" -e "s,$RETDECDIR,RETDECDIR,g" >$dir/by_retdec.c
       else
-	  echo "  failed"
-	  if grep "You have reached your limit" out >/dev/null; then
-	  	echo "=== limit reached; aborting"
-	  	rm -f out "$cfile"
-	  	exit 1
-	  fi
-	  touch $dir/by_retdec.failed
+        echo "  failed"
+        touch $dir/by_retdec.failed
       fi
-      sed <out -e "s,$root/,," >$dir/by_retdec.out
+      sed <out -e "s,$root/,,g" -e "s,$RETDECDIR,RETDECDIR,g" >$dir/by_retdec.out
   fi
   rm -f "$cfile"
 done
