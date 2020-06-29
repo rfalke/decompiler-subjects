091f640ba007de5227081bea3397f47d72813b0f
Raimar Falke <i-git-stone@rf.risimo.net>
2020-06-29 21:36:51 +0200

    Update script

--- scripts/show_stats.sh
+++ scripts/show_stats.sh
@@ -3,16 +3,16 @@
 set -e
 
 echo "=== Architecture"
-find . -name 'subject.exe' | rev | cut -d'/' -f2 | rev | cut -d_ -f1 | sort | uniq -c 
+find . -name 'subject.exe' | rev | cut -d'/' -f2 | rev | cut -d_ -f1 | sort | uniq -c | sort -n
 echo
 
 echo "=== File formats"
-find . -name 'subject.exe' | rev | cut -d'/' -f2 | rev | cut -d_ -f2 | sort | uniq -c 
+find . -name 'subject.exe' | rev | cut -d'/' -f2 | rev | cut -d_ -f2 | sort | uniq -c | sort -n
 echo
 
 printf "===     decompiler   | failed | success\n"
 
-for decomp in boomerang rec16 rec4 holdec dcc nemerle_dcc decompiler.sf.net reko snowman retdec hex-rays
+for decomp in boomerang boomerang-ng rec16 rec4 holdec dcc nemerle_dcc decompiler.sf.net reko snowman retdec hex-rays ghidra
 do
   ok=$(find . -name "by_$decomp.c" | wc -l)
   failed=$(find . -name "by_$decomp.failed" | wc -l)
