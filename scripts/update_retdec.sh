4fea72f556318cf910c5005bcdf49b956ac40dfa
Raimar Falke <i-git-stone@rf.risimo.net>
2020-07-02 10:11:15 +0200

    Partial run with self compiled retdec (git hash 56f66c7)

--- scripts/update_retdec.sh
+++ scripts/update_retdec.sh
@@ -13,23 +13,24 @@ fi
 
 root=$(pwd)
 
-find $root -name subject.exe | grep -v amiga | grep -v sparc | grep -v _mz_ | grep -v x64 | grep -v arm64 | grep -v sharedexec | grep -v from_pouet.net | grep -v malware_via_email | grep holdec | grep dmi | sort | while read line
+find $root -name subject.exe | grep -v amiga | grep -v sparc | grep -v _mz_ | grep -v sharedexec | grep -v from_pouet.net | grep -v malware_via_email | sort | while read line
 do
   dir=$(dirname $line)
-  cfile=$(echo $line | sed 's,/subject.exe,/subject.c,')
+  cfile=$(echo $line | sed 's,/subject.exe,/retdec_subject.c,')
   if test -f $dir/by_retdec.c -o -f $dir/by_retdec.failed; then
       echo "skipping $line" >/dev/null
   else
       echo -n "decompiling $line"
-      rm -f $dir/by_retdec.c $dir/by_retdec.failed
-      if $RETDECDIR/bin/retdec-decompiler.py --no-config --cleanup --no-config --no-memory-limit -o "$cfile" "$line" >out 2>&1 ; then
+      rm -f $dir/by_retdec.c $dir/by_retdec.failed $dir/retdec_subject.*
+      $RETDECDIR/bin/retdec-decompiler --cleanup --no-memory-limit -o "$cfile" "$line" >out 2>&1 || true
+      if test -s "$cfile" ; then
         echo "  ok"
         sed <$cfile -e "s,$root/,,g" -e "s,$RETDECDIR,RETDECDIR,g" >$dir/by_retdec.c
       else
         echo "  failed"
         touch $dir/by_retdec.failed
       fi
-      sed <out -e "s,$root/,,g" -e "s,$RETDECDIR,RETDECDIR,g" >$dir/by_retdec.out
+      sed <out -e "s,$root/,,g" -e "s,$RETDECDIR,RETDECDIR,g" -e 's/\x1b\[[0-9;]*m//g' -e 's/[0-9]\+[.][0-9][0-9]s/some time/g' >$dir/by_retdec.out
   fi
-  rm -f "$cfile"
+  rm -f $dir/retdec_subject.*
 done
