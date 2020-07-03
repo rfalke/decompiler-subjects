f5ad49341d094435c09c34cbb3c436cd484c3d4a
Raimar Falke <i-git-stone@rf.risimo.net>
2020-07-03 18:33:29 +0200

    Full run of last available rec4 version (Sep 23 2015)

--- scripts/update_rec4.sh
+++ scripts/update_rec4.sh
@@ -1,45 +1,44 @@
 #!/bin/sh
 
 set -e
-ulimit -c 0
-ulimit -t 120
+#set -x
 
-if test -z "$RECBINDIR"; then
-    echo "\$RECBINDIR not set. This script executes
-the \$RECBINDIR/RecStudio4CLI file."
+ulimit -c 0
+ulimit -St 600
+ulimit -Ht unlimited
+ulimit -v 4000000
+
+if test -z "$RECDIR"; then
+    echo "\$RECDIR not set. This script executes the \$RECDIR/bin/RecCLI file."
     exit 1
 fi
 
-if test "$1" = "-a"; then
-	decompileAll="1"
-else
-	decompileAll=""
-fi
+root=$(pwd)
 
-find . -name subject.exe | sort | grep -v ppc_macho | grep -v hppa | while read line
+find . -name subject.exe | sort | grep -v ppc_macho | grep -v hppa | grep -v arm64 | grep -v m68k_amiga | grep -v ia32_mz | grep -v ia32_com | while read line
 do
   dir=$(dirname $line)
   dir=`pwd`/$dir
   
-  if test -z "$decompileAll" -a \( -f $dir/by_rec4.c -o -f $dir/by_rec4.failed \); then
+  if test -f $dir/by_rec4.c -o -f $dir/by_rec4.failed ; then
     echo "skipping $line" >/dev/null
   else
     /bin/echo -n "decompiling $line"
-    rm -f $dir/by_rec4.c $dir/by_rec4.failed $dir/by_rec4.out
-    if $RECBINDIR/RecStudio4CLI "$line" "$dir/by_rec4.c" > "$dir/by_rec4.outRaw" 2>&1 ; then
-        if test -f "$dir/by_rec4.c"; then
-	      echo "  ok"
-          else
-          echo "no .c file"
-          exit 1
-        fi
+    rm -f $dir/by_rec4.c $dir/by_rec4.failed $dir/by_rec4.out $dir/by_rec4.outRaw
+    $RECDIR/bin/RecCLI "$line" "$dir/by_rec4.c" > "$dir/by_rec4.outRaw" 2>&1  || true
+    if test -f "$dir/by_rec4.c"; then
+      if grep ' 0 Register nodes' "$dir/by_rec4.c" >/dev/null ; then
+        echo "  failed (empty output)"
+        rm $dir/by_rec4.c
+        touch $dir/by_rec4.failed
+      else
+      	echo "  ok"
+      fi
     else
         echo "  failed"
         touch $dir/by_rec4.failed
     fi
-    grep -v "Elapsed time" <"$dir/by_rec4.outRaw" >"$dir/by_rec4.out"
+    (grep -v "Elapsed time" <"$dir/by_rec4.outRaw" | grep -v 'cwd:' >"$dir/by_rec4.out") || true
     rm "$dir/by_rec4.outRaw"
   fi
 done
-
-find . -name by_rec4.failed -print
