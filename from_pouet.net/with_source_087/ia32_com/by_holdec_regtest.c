48d89bb66874b1a40f2d5a69006a3e0d41878144
Raimar Falke <i-git-stone@rf.risimo.net>
2020-07-14 00:08:09 +0200

    Add 2 16bit COM files as holdec regression test

---
+++ from_pouet.net/with_source_087/ia32_com/by_holdec_regtest.c
@@ -0,0 +1,37 @@
+// Decompiled by holdec
+
+// address: 00000100.0
+// full-signature: func(main, return=[<int(undef, 2),null,unknown>], parameter=[], varargs=false)
+d2 main(void)
+{
+  reg_var1 = -1536;
+  reg_var2 = 0.0;
+  do {
+    reg_var1a = reg_var1;
+    (void) STORE(UNSIGNED_EXTEND(reg_var1), FLOAT_TO_INT(34.0 * SIN(reg_var2) + 68.0));
+    reg_var2 += 0.01227184630308513;
+    reg_af = reg_var1 + 1;
+    reg_var1++;
+  } while(reg_af != 0);
+  reg_ah = setVideoMode(19);
+  reg_var3 = WIDEN(reg_ah);
+  while(1) {
+    reg_var4 = 255;
+    reg_var5 = reg_var1a + 1;
+    reg_var7 = reg_var3 + 1;
+    reg_var7a = reg_var7;
+    reg_var6 = NARROW(reg_var7);
+    do {
+      reg_as = WIDEN(LOAD(UNSIGNED_EXTEND(reg_var6) + 0xfa00)) | 0xfa00;
+      reg_var7++;
+      reg_var6 = NARROW(reg_var7);
+      reg_av = UNSIGNED_EXTEND(reg_var4 & 0xff00 | 0xa0);
+      reg_aw = reg_var4 & 0xff00 | 0xa0;
+      reg_var4 = 0;
+      (void) _holdec_memcpy(UNSIGNED_EXTEND(reg_as), SEGMENT2ADDR(-24576) + UNSIGNED_EXTEND(reg_var5), reg_av * 2);
+      reg_var5 += reg_aw;
+    } while(reg_var5 < -1536 || reg_var5 > -1);
+    reg_var3 = reg_var7a;
+  }
+}
+
---
+++ from_pouet.net/with_source_364/ia32_com/by_holdec_regtest.c
@@ -0,0 +1,41 @@
+// Decompiled by holdec
+
+// address: 00000505.0
+// full-signature: func(do_adlib_io, return=[], parameter=[<int(undef, 1),cmd1,reg[al]>, <int(undef, 1),cmd2,reg[ah]>], varargs=false)
+void do_adlib_io(d1 cmd1, d1 cmd2)
+{
+  (void) _holdec_ia32_outB(904, cmd1);
+  reg_var1 = 6;
+  do {
+    (void) _holdec_ia32_inB(904);
+    reg_ac = reg_var1 + -1;
+    reg_var1--;
+  } while(reg_ac != 0);
+  (void) _holdec_ia32_outB(905, cmd2);
+  reg_var2 = reg_var1 & 0xff00 | 0x23;
+  do {
+    (void) _holdec_ia32_inB(904);
+    reg_ag = reg_var2 + -1;
+    reg_var2--;
+  } while(reg_ag != 0);
+}
+
+// address: 00000ef2.0
+// full-signature: func(copy_with_or, return=[], parameter=[<int(undef, 2),src,reg[si]>, <int(undef, 2),dest,reg[di]>, <int(undef, 2),num_words,reg[cx]>, <int(undef, 1),value_to_or,reg[bl]>], varargs=false)
+void copy_with_or(d2 src, d2 dest, d2 num_words, d1 value_to_or)
+{
+  reg_var2 = src;
+  reg_var1 = dest;
+  reg_var3 = num_words;
+  if(num_words != 0) {
+    do {
+      reg_ad = LOAD(UNSIGNED_EXTEND(reg_var2));
+      reg_var2 += 2;
+      (void) STORE(UNSIGNED_EXTEND(reg_var1), reg_ad & 0xff | WIDEN(NARROW(reg_ad / 256) | value_to_or) * 256);
+      reg_var1 += 2;
+      reg_ag = reg_var3 + -1;
+      reg_var3--;
+    } while(reg_ag != 0);
+  }
+}
+
