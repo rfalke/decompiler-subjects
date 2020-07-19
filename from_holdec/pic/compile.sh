d30fffc13a655a65034d4774b611cad6db6c9d5e
Raimar Falke <i-git-stone@rf.risimo.net>
2020-07-20 01:13:32 +0200

    Adjust dirnames for case insensitive filesystems

--- from_holdec/pic/compile.sh
+++ from_holdec/pic/compile.sh
@@ -11,17 +11,24 @@ do
 			echo "=== $opt $cc $pic"
 			if test "$pic" = "plain"; then
 				pic2=""
+				dname="plain"
 			else
 				pic2="-f$pic"
+				lower=$(echo $pic | tr '[:upper:]' '[:lower:]')
+				if test "$pic" = "$lower"; then
+					dname="limited-$lower"
+				else
+					dname="unlimited-$lower"
+				fi
 			fi
 			opt2="$opt -m32"
 			arch="ia32"
-			dir="${arch}_elf_${cc}_${opt}_${pic}"
+			dir="${arch}_elf_${cc}_${opt}_${dname}"
 			mkdir -p "$dir"
 			$cc $baseopt -$opt2 $pic2 -g source.c -o "$dir/subject.exe"
 
 			arch="x64"
-			dir="${arch}_elf_${cc}_${opt}_${pic}"
+			dir="${arch}_elf_${cc}_${opt}_${dname}"
 			mkdir -p "$dir"
 			$cc $baseop -$opt $pic2 -g source.c -o "$dir/subject.exe"
 		done
