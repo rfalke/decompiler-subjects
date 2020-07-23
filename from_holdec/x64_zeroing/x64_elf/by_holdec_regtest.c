9181878c903c7fbb71ed6387b5590281726c4aac
Raimar Falke <i-git-stone@rf.risimo.net>
2020-07-23 19:43:50 +0200

    Add holdec regression test

---
+++ from_holdec/x64_zeroing/x64_elf/by_holdec_regtest.c
@@ -0,0 +1,11 @@
+// Decompiled by holdec
+
+// address: 00401040.0
+// full-signature: func(main, return=[<int(signed, 4),null,unknown>], parameter=[<int(signed, 4),argc,unknown>, <ptr(ptr(int(undef, 1))),argv,unknown>], varargs=false)
+s4 main(s4 argc, d1** argv)
+{
+  (void) printf("mov eax: 0x%016lx\n", 30292);
+  (void) printf("mov  ax: 0x%016lx\n", 1234605616436508244);
+  return 0;
+}
+
