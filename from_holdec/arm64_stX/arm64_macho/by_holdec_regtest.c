d53b6a2c8453c95a0e5281ef837d6840fd05e834
Raimar Falke <i-git-stone@rf.risimo.net>
2021-09-11 22:25:24 +0200

    Add regression test for new stX subject

---
+++ from_holdec/arm64_stX/arm64_macho/by_holdec_regtest.c
@@ -0,0 +1,39 @@
+// Decompiled by holdec
+
+// address: 0100002eb0.0
+// full-signature: func(_full_write_with_str, return=[], parameter=[<ptr(int(undef, 1)),dest,reg[x0]>], varargs=false)
+void _full_write_with_str(d1* dest)
+{
+  (void) STORE(dest, 41362427191743139026751447860679676176);
+}
+
+// address: 0100002f04.0
+// full-signature: func(_full_store_with_byte_wise_st1, return=[], parameter=[<ptr(int(undef, 1)),dest,reg[x0]>], varargs=false)
+void _full_store_with_byte_wise_st1(d1* dest)
+{
+  (void) STORE(dest, 16);
+  (void) STORE(dest + 1, 17);
+  (void) STORE(dest + 2, 18);
+  (void) STORE(dest + 3, 19);
+  (void) STORE(dest + 4, 20);
+  (void) STORE(dest + 5, 21);
+  (void) STORE(dest + 6, 22);
+  (void) STORE(dest + 7, 23);
+  (void) STORE(dest + 8, 24);
+  (void) STORE(dest + 9, 25);
+  (void) STORE(dest + 10, 26);
+  (void) STORE(dest + 11, 27);
+  (void) STORE(dest + 12, 28);
+  (void) STORE(dest + 13, 29);
+  (void) STORE(dest + 14, 30);
+  (void) STORE(dest + 15, 31);
+}
+
+// address: 0100002f58.0
+// full-signature: func(_full_store_with_8byte_wise_st1, return=[], parameter=[<ptr(int(undef, 1)),dest,reg[x0]>], varargs=false)
+void _full_store_with_8byte_wise_st1(d1* dest)
+{
+  (void) STORE(dest + 8, 2242261671028070680);
+  (void) STORE(dest, 1663540288323457296);
+}
+
