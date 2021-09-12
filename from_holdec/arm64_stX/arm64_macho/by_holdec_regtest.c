19e190b05a927344142a511a2ccdfeca98b1db74
Raimar Falke <i-git-stone@rf.risimo.net>
2021-09-12 13:12:04 +0200

    Add more methods to regression test

--- from_holdec/arm64_stX/arm64_macho/by_holdec_regtest.c
+++ from_holdec/arm64_stX/arm64_macho/by_holdec_regtest.c
@@ -37,3 +37,508 @@ void _full_store_with_8byte_wise_st1(d1* dest)
   (void) STORE(dest, 1663540288323457296);
 }
 
+// address: 0100003000.0
+// full-signature: func(_full_store_2_regs_with_byte_wise_st1, return=[], parameter=[<ptr(int(undef, 1)),dest,reg[x0]>], varargs=false)
+void _full_store_2_regs_with_byte_wise_st1(d1* dest)
+{
+  (void) STORE(dest, 16);
+  (void) STORE(dest + 1, 17);
+  (void) STORE(dest + 2, 18);
+  (void) STORE(dest + 3, 19);
+  (void) STORE(dest + 4, 20);
+  (void) STORE(dest + 5, 21);
+  (void) STORE(dest + 6, 22);
+  (void) STORE(dest + 7, 23);
+  (void) STORE(dest + 8, 24);
+  (void) STORE(dest + 9, 25);
+  (void) STORE(dest + 10, 26);
+  (void) STORE(dest + 11, 27);
+  (void) STORE(dest + 12, 28);
+  (void) STORE(dest + 13, 29);
+  (void) STORE(dest + 14, 30);
+  (void) STORE(dest + 15, 31);
+  (void) STORE(dest + 16, 32);
+  (void) STORE(dest + 17, 33);
+  (void) STORE(dest + 18, 34);
+  (void) STORE(dest + 19, 35);
+  (void) STORE(dest + 20, 36);
+  (void) STORE(dest + 21, 37);
+  (void) STORE(dest + 22, 38);
+  (void) STORE(dest + 23, 39);
+  (void) STORE(dest + 24, 40);
+  (void) STORE(dest + 25, 41);
+  (void) STORE(dest + 26, 42);
+  (void) STORE(dest + 27, 43);
+  (void) STORE(dest + 28, 44);
+  (void) STORE(dest + 29, 45);
+  (void) STORE(dest + 30, 46);
+  (void) STORE(dest + 31, 47);
+}
+
+// address: 0100003054.0
+// full-signature: func(_full_store_4_regs_with_byte_wise_st1, return=[], parameter=[<ptr(int(undef, 1)),dest,reg[x0]>], varargs=false)
+void _full_store_4_regs_with_byte_wise_st1(d1* dest)
+{
+  (void) STORE(dest, 16);
+  (void) STORE(dest + 1, 17);
+  (void) STORE(dest + 2, 18);
+  (void) STORE(dest + 3, 19);
+  (void) STORE(dest + 4, 20);
+  (void) STORE(dest + 5, 21);
+  (void) STORE(dest + 6, 22);
+  (void) STORE(dest + 7, 23);
+  (void) STORE(dest + 8, 24);
+  (void) STORE(dest + 9, 25);
+  (void) STORE(dest + 10, 26);
+  (void) STORE(dest + 11, 27);
+  (void) STORE(dest + 12, 28);
+  (void) STORE(dest + 13, 29);
+  (void) STORE(dest + 14, 30);
+  (void) STORE(dest + 15, 31);
+  (void) STORE(dest + 16, 32);
+  (void) STORE(dest + 17, 33);
+  (void) STORE(dest + 18, 34);
+  (void) STORE(dest + 19, 35);
+  (void) STORE(dest + 20, 36);
+  (void) STORE(dest + 21, 37);
+  (void) STORE(dest + 22, 38);
+  (void) STORE(dest + 23, 39);
+  (void) STORE(dest + 24, 40);
+  (void) STORE(dest + 25, 41);
+  (void) STORE(dest + 26, 42);
+  (void) STORE(dest + 27, 43);
+  (void) STORE(dest + 28, 44);
+  (void) STORE(dest + 29, 45);
+  (void) STORE(dest + 30, 46);
+  (void) STORE(dest + 31, 47);
+  (void) STORE(dest + 32, 48);
+  (void) STORE(dest + 33, 49);
+  (void) STORE(dest + 34, 50);
+  (void) STORE(dest + 35, 51);
+  (void) STORE(dest + 36, 52);
+  (void) STORE(dest + 37, 53);
+  (void) STORE(dest + 38, 54);
+  (void) STORE(dest + 39, 55);
+  (void) STORE(dest + 40, 56);
+  (void) STORE(dest + 41, 57);
+  (void) STORE(dest + 42, 58);
+  (void) STORE(dest + 43, 59);
+  (void) STORE(dest + 44, 60);
+  (void) STORE(dest + 45, 61);
+  (void) STORE(dest + 46, 62);
+  (void) STORE(dest + 47, 63);
+  (void) STORE(dest + 48, 64);
+  (void) STORE(dest + 49, 65);
+  (void) STORE(dest + 50, 66);
+  (void) STORE(dest + 51, 67);
+  (void) STORE(dest + 52, 68);
+  (void) STORE(dest + 53, 69);
+  (void) STORE(dest + 54, 70);
+  (void) STORE(dest + 55, 71);
+  (void) STORE(dest + 56, 72);
+  (void) STORE(dest + 57, 73);
+  (void) STORE(dest + 58, 74);
+  (void) STORE(dest + 59, 75);
+  (void) STORE(dest + 60, 76);
+  (void) STORE(dest + 61, 77);
+  (void) STORE(dest + 62, 78);
+  (void) STORE(dest + 63, 79);
+}
+
+// address: 01000030a8.0
+// full-signature: func(_indexed_st1__one_byte_from_index_0, return=[], parameter=[<ptr(int(undef, 1)),dest,reg[x0]>], varargs=false)
+void _indexed_st1__one_byte_from_index_0(d1* dest)
+{
+  (void) STORE(dest, 16);
+}
+
+// address: 01000030fc.0
+// full-signature: func(_indexed_st1__one_byte_from_index_3, return=[], parameter=[<ptr(int(undef, 1)),dest,reg[x0]>], varargs=false)
+void _indexed_st1__one_byte_from_index_3(d1* dest)
+{
+  (void) STORE(dest, 19);
+}
+
+// address: 0100003150.0
+// full-signature: func(_indexed_st1__8_bytes_from_low, return=[], parameter=[<ptr(int(undef, 1)),dest,reg[x0]>], varargs=false)
+void _indexed_st1__8_bytes_from_low(d1* dest)
+{
+  (void) STORE(dest, 1663540288323457296);
+}
+
+// address: 01000031a4.0
+// full-signature: func(_indexed_st1__8_bytes_from_high, return=[], parameter=[<ptr(int(undef, 1)),dest,reg[x0]>], varargs=false)
+void _indexed_st1__8_bytes_from_high(d1* dest)
+{
+  (void) STORE(dest, 2242261671028070680);
+}
+
+// address: 01000031f8.0
+// full-signature: func(_write_interleaved_bytes_from_2_regs, return=[], parameter=[<ptr(int(undef, 1)),dest,reg[x0]>], varargs=false)
+void _write_interleaved_bytes_from_2_regs(d1* dest)
+{
+  (void) STORE(dest, 16);
+  (void) STORE(dest + 1, 32);
+  (void) STORE(dest + 2, 17);
+  (void) STORE(dest + 3, 33);
+  (void) STORE(dest + 4, 18);
+  (void) STORE(dest + 5, 34);
+  (void) STORE(dest + 6, 19);
+  (void) STORE(dest + 7, 35);
+  (void) STORE(dest + 8, 20);
+  (void) STORE(dest + 9, 36);
+  (void) STORE(dest + 10, 21);
+  (void) STORE(dest + 11, 37);
+  (void) STORE(dest + 12, 22);
+  (void) STORE(dest + 13, 38);
+  (void) STORE(dest + 14, 23);
+  (void) STORE(dest + 15, 39);
+  (void) STORE(dest + 16, 24);
+  (void) STORE(dest + 17, 40);
+  (void) STORE(dest + 18, 25);
+  (void) STORE(dest + 19, 41);
+  (void) STORE(dest + 20, 26);
+  (void) STORE(dest + 21, 42);
+  (void) STORE(dest + 22, 27);
+  (void) STORE(dest + 23, 43);
+  (void) STORE(dest + 24, 28);
+  (void) STORE(dest + 25, 44);
+  (void) STORE(dest + 26, 29);
+  (void) STORE(dest + 27, 45);
+  (void) STORE(dest + 28, 30);
+  (void) STORE(dest + 29, 46);
+  (void) STORE(dest + 30, 31);
+  (void) STORE(dest + 31, 47);
+}
+
+// address: 010000324c.0
+// full-signature: func(_write_interleaved_bytes_from_2_regs__half_, return=[], parameter=[<ptr(int(undef, 1)),dest,reg[x0]>], varargs=false)
+void _write_interleaved_bytes_from_2_regs__half_(d1* dest)
+{
+  (void) STORE(dest, 16);
+  (void) STORE(dest + 1, 32);
+  (void) STORE(dest + 2, 17);
+  (void) STORE(dest + 3, 33);
+  (void) STORE(dest + 4, 18);
+  (void) STORE(dest + 5, 34);
+  (void) STORE(dest + 6, 19);
+  (void) STORE(dest + 7, 35);
+  (void) STORE(dest + 8, 20);
+  (void) STORE(dest + 9, 36);
+  (void) STORE(dest + 10, 21);
+  (void) STORE(dest + 11, 37);
+  (void) STORE(dest + 12, 22);
+  (void) STORE(dest + 13, 38);
+  (void) STORE(dest + 14, 23);
+  (void) STORE(dest + 15, 39);
+}
+
+// address: 01000032a0.0
+// full-signature: func(_write_interleaved_8_bytes_from_2_regs, return=[], parameter=[<ptr(int(undef, 1)),dest,reg[x0]>], varargs=false)
+void _write_interleaved_8_bytes_from_2_regs(d1* dest)
+{
+  (void) STORE(dest + 8, 2820983053732684064);
+  (void) STORE(dest + 16, 2242261671028070680);
+  (void) STORE(dest + 24, 3399704436437297448);
+  (void) STORE(dest, 1663540288323457296);
+}
+
+// address: 01000032f4.0
+// full-signature: func(_indexed_st2__write_2_bytes_from_2_regs, return=[], parameter=[<ptr(int(undef, 1)),dest,reg[x0]>], varargs=false)
+void _indexed_st2__write_2_bytes_from_2_regs(d1* dest)
+{
+  (void) STORE(dest, 25);
+  (void) STORE(dest + 1, 41);
+}
+
+// address: 0100003348.0
+// full-signature: func(_indexed_st2__8_bytes_from_lows, return=[], parameter=[<ptr(int(undef, 1)),dest,reg[x0]>], varargs=false)
+void _indexed_st2__8_bytes_from_lows(d1* dest)
+{
+  (void) STORE(dest + 8, 2820983053732684064);
+  (void) STORE(dest, 1663540288323457296);
+}
+
+// address: 010000339c.0
+// full-signature: func(_indexed_st2__8_bytes_from_highs, return=[], parameter=[<ptr(int(undef, 1)),dest,reg[x0]>], varargs=false)
+void _indexed_st2__8_bytes_from_highs(d1* dest)
+{
+  (void) STORE(dest + 8, 3399704436437297448);
+  (void) STORE(dest, 2242261671028070680);
+}
+
+// address: 01000033f0.0
+// full-signature: func(_write_interleaved_bytes_from_3_regs, return=[], parameter=[<ptr(int(undef, 1)),dest,reg[x0]>], varargs=false)
+void _write_interleaved_bytes_from_3_regs(d1* dest)
+{
+  (void) STORE(dest, 16);
+  (void) STORE(dest + 1, 32);
+  (void) STORE(dest + 2, 48);
+  (void) STORE(dest + 3, 17);
+  (void) STORE(dest + 4, 33);
+  (void) STORE(dest + 5, 49);
+  (void) STORE(dest + 6, 18);
+  (void) STORE(dest + 7, 34);
+  (void) STORE(dest + 8, 50);
+  (void) STORE(dest + 9, 19);
+  (void) STORE(dest + 10, 35);
+  (void) STORE(dest + 11, 51);
+  (void) STORE(dest + 12, 20);
+  (void) STORE(dest + 13, 36);
+  (void) STORE(dest + 14, 52);
+  (void) STORE(dest + 15, 21);
+  (void) STORE(dest + 16, 37);
+  (void) STORE(dest + 17, 53);
+  (void) STORE(dest + 18, 22);
+  (void) STORE(dest + 19, 38);
+  (void) STORE(dest + 20, 54);
+  (void) STORE(dest + 21, 23);
+  (void) STORE(dest + 22, 39);
+  (void) STORE(dest + 23, 55);
+  (void) STORE(dest + 24, 24);
+  (void) STORE(dest + 25, 40);
+  (void) STORE(dest + 26, 56);
+  (void) STORE(dest + 27, 25);
+  (void) STORE(dest + 28, 41);
+  (void) STORE(dest + 29, 57);
+  (void) STORE(dest + 30, 26);
+  (void) STORE(dest + 31, 42);
+  (void) STORE(dest + 32, 58);
+  (void) STORE(dest + 33, 27);
+  (void) STORE(dest + 34, 43);
+  (void) STORE(dest + 35, 59);
+  (void) STORE(dest + 36, 28);
+  (void) STORE(dest + 37, 44);
+  (void) STORE(dest + 38, 60);
+  (void) STORE(dest + 39, 29);
+  (void) STORE(dest + 40, 45);
+  (void) STORE(dest + 41, 61);
+  (void) STORE(dest + 42, 30);
+  (void) STORE(dest + 43, 46);
+  (void) STORE(dest + 44, 62);
+  (void) STORE(dest + 45, 31);
+  (void) STORE(dest + 46, 47);
+  (void) STORE(dest + 47, 63);
+}
+
+// address: 0100003444.0
+// full-signature: func(_write_interleaved_bytes_from_3_regs__half_, return=[], parameter=[<ptr(int(undef, 1)),dest,reg[x0]>], varargs=false)
+void _write_interleaved_bytes_from_3_regs__half_(d1* dest)
+{
+  (void) STORE(dest, 16);
+  (void) STORE(dest + 1, 32);
+  (void) STORE(dest + 2, 48);
+  (void) STORE(dest + 3, 17);
+  (void) STORE(dest + 4, 33);
+  (void) STORE(dest + 5, 49);
+  (void) STORE(dest + 6, 18);
+  (void) STORE(dest + 7, 34);
+  (void) STORE(dest + 8, 50);
+  (void) STORE(dest + 9, 19);
+  (void) STORE(dest + 10, 35);
+  (void) STORE(dest + 11, 51);
+  (void) STORE(dest + 12, 20);
+  (void) STORE(dest + 13, 36);
+  (void) STORE(dest + 14, 52);
+  (void) STORE(dest + 15, 21);
+  (void) STORE(dest + 16, 37);
+  (void) STORE(dest + 17, 53);
+  (void) STORE(dest + 18, 22);
+  (void) STORE(dest + 19, 38);
+  (void) STORE(dest + 20, 54);
+  (void) STORE(dest + 21, 23);
+  (void) STORE(dest + 22, 39);
+  (void) STORE(dest + 23, 55);
+}
+
+// address: 0100003498.0
+// full-signature: func(_write_interleaved_8_bytes_from_3_regs, return=[], parameter=[<ptr(int(undef, 1)),dest,reg[x0]>], varargs=false)
+void _write_interleaved_8_bytes_from_3_regs(d1* dest)
+{
+  (void) STORE(dest + 8, 2820983053732684064);
+  (void) STORE(dest + 16, 3978425819141910832);
+  (void) STORE(dest + 24, 2242261671028070680);
+  (void) STORE(dest + 32, 3399704436437297448);
+  (void) STORE(dest + 40, 4557147201846524216);
+  (void) STORE(dest, 1663540288323457296);
+}
+
+// address: 01000034ec.0
+// full-signature: func(_indexed_st3__write_3_bytes_from_3_regs, return=[], parameter=[<ptr(int(undef, 1)),dest,reg[x0]>], varargs=false)
+void _indexed_st3__write_3_bytes_from_3_regs(d1* dest)
+{
+  (void) STORE(dest, 25);
+  (void) STORE(dest + 1, 41);
+  (void) STORE(dest + 2, 57);
+}
+
+// address: 0100003540.0
+// full-signature: func(_indexed_st3__8_bytes_from_lows, return=[], parameter=[<ptr(int(undef, 1)),dest,reg[x0]>], varargs=false)
+void _indexed_st3__8_bytes_from_lows(d1* dest)
+{
+  (void) STORE(dest + 8, 2820983053732684064);
+  (void) STORE(dest + 16, 3978425819141910832);
+  (void) STORE(dest, 1663540288323457296);
+}
+
+// address: 0100003594.0
+// full-signature: func(_indexed_st3__8_bytes_from_highs, return=[], parameter=[<ptr(int(undef, 1)),dest,reg[x0]>], varargs=false)
+void _indexed_st3__8_bytes_from_highs(d1* dest)
+{
+  (void) STORE(dest + 8, 3399704436437297448);
+  (void) STORE(dest + 16, 4557147201846524216);
+  (void) STORE(dest, 2242261671028070680);
+}
+
+// address: 01000035e8.0
+// full-signature: func(_write_interleaved_bytes_from_4_regs, return=[], parameter=[<ptr(int(undef, 1)),dest,reg[x0]>], varargs=false)
+void _write_interleaved_bytes_from_4_regs(d1* dest)
+{
+  (void) STORE(dest, 16);
+  (void) STORE(dest + 1, 32);
+  (void) STORE(dest + 2, 48);
+  (void) STORE(dest + 3, 64);
+  (void) STORE(dest + 4, 17);
+  (void) STORE(dest + 5, 33);
+  (void) STORE(dest + 6, 49);
+  (void) STORE(dest + 7, 65);
+  (void) STORE(dest + 8, 18);
+  (void) STORE(dest + 9, 34);
+  (void) STORE(dest + 10, 50);
+  (void) STORE(dest + 11, 66);
+  (void) STORE(dest + 12, 19);
+  (void) STORE(dest + 13, 35);
+  (void) STORE(dest + 14, 51);
+  (void) STORE(dest + 15, 67);
+  (void) STORE(dest + 16, 20);
+  (void) STORE(dest + 17, 36);
+  (void) STORE(dest + 18, 52);
+  (void) STORE(dest + 19, 68);
+  (void) STORE(dest + 20, 21);
+  (void) STORE(dest + 21, 37);
+  (void) STORE(dest + 22, 53);
+  (void) STORE(dest + 23, 69);
+  (void) STORE(dest + 24, 22);
+  (void) STORE(dest + 25, 38);
+  (void) STORE(dest + 26, 54);
+  (void) STORE(dest + 27, 70);
+  (void) STORE(dest + 28, 23);
+  (void) STORE(dest + 29, 39);
+  (void) STORE(dest + 30, 55);
+  (void) STORE(dest + 31, 71);
+  (void) STORE(dest + 32, 24);
+  (void) STORE(dest + 33, 40);
+  (void) STORE(dest + 34, 56);
+  (void) STORE(dest + 35, 72);
+  (void) STORE(dest + 36, 25);
+  (void) STORE(dest + 37, 41);
+  (void) STORE(dest + 38, 57);
+  (void) STORE(dest + 39, 73);
+  (void) STORE(dest + 40, 26);
+  (void) STORE(dest + 41, 42);
+  (void) STORE(dest + 42, 58);
+  (void) STORE(dest + 43, 74);
+  (void) STORE(dest + 44, 27);
+  (void) STORE(dest + 45, 43);
+  (void) STORE(dest + 46, 59);
+  (void) STORE(dest + 47, 75);
+  (void) STORE(dest + 48, 28);
+  (void) STORE(dest + 49, 44);
+  (void) STORE(dest + 50, 60);
+  (void) STORE(dest + 51, 76);
+  (void) STORE(dest + 52, 29);
+  (void) STORE(dest + 53, 45);
+  (void) STORE(dest + 54, 61);
+  (void) STORE(dest + 55, 77);
+  (void) STORE(dest + 56, 30);
+  (void) STORE(dest + 57, 46);
+  (void) STORE(dest + 58, 62);
+  (void) STORE(dest + 59, 78);
+  (void) STORE(dest + 60, 31);
+  (void) STORE(dest + 61, 47);
+  (void) STORE(dest + 62, 63);
+  (void) STORE(dest + 63, 79);
+}
+
+// address: 010000363c.0
+// full-signature: func(_write_interleaved_bytes_from_4_regs__half_, return=[], parameter=[<ptr(int(undef, 1)),dest,reg[x0]>], varargs=false)
+void _write_interleaved_bytes_from_4_regs__half_(d1* dest)
+{
+  (void) STORE(dest, 16);
+  (void) STORE(dest + 1, 32);
+  (void) STORE(dest + 2, 48);
+  (void) STORE(dest + 3, 64);
+  (void) STORE(dest + 4, 17);
+  (void) STORE(dest + 5, 33);
+  (void) STORE(dest + 6, 49);
+  (void) STORE(dest + 7, 65);
+  (void) STORE(dest + 8, 18);
+  (void) STORE(dest + 9, 34);
+  (void) STORE(dest + 10, 50);
+  (void) STORE(dest + 11, 66);
+  (void) STORE(dest + 12, 19);
+  (void) STORE(dest + 13, 35);
+  (void) STORE(dest + 14, 51);
+  (void) STORE(dest + 15, 67);
+  (void) STORE(dest + 16, 20);
+  (void) STORE(dest + 17, 36);
+  (void) STORE(dest + 18, 52);
+  (void) STORE(dest + 19, 68);
+  (void) STORE(dest + 20, 21);
+  (void) STORE(dest + 21, 37);
+  (void) STORE(dest + 22, 53);
+  (void) STORE(dest + 23, 69);
+  (void) STORE(dest + 24, 22);
+  (void) STORE(dest + 25, 38);
+  (void) STORE(dest + 26, 54);
+  (void) STORE(dest + 27, 70);
+  (void) STORE(dest + 28, 23);
+  (void) STORE(dest + 29, 39);
+  (void) STORE(dest + 30, 55);
+  (void) STORE(dest + 31, 71);
+}
+
+// address: 0100003690.0
+// full-signature: func(_write_interleaved_8_bytes_from_4_regs, return=[], parameter=[<ptr(int(undef, 1)),dest,reg[x0]>], varargs=false)
+void _write_interleaved_8_bytes_from_4_regs(d1* dest)
+{
+  (void) STORE(dest + 8, 2820983053732684064);
+  (void) STORE(dest + 16, 3978425819141910832);
+  (void) STORE(dest + 24, 5135868584551137600);
+  (void) STORE(dest + 32, 2242261671028070680);
+  (void) STORE(dest + 40, 3399704436437297448);
+  (void) STORE(dest + 48, 4557147201846524216);
+  (void) STORE(dest + 56, 5714589967255750984);
+  (void) STORE(dest, 1663540288323457296);
+}
+
+// address: 01000036e4.0
+// full-signature: func(_indexed_st4__write_4_bytes_from_4_regs, return=[], parameter=[<ptr(int(undef, 1)),dest,reg[x0]>], varargs=false)
+void _indexed_st4__write_4_bytes_from_4_regs(d1* dest)
+{
+  (void) STORE(dest, 25);
+  (void) STORE(dest + 1, 41);
+  (void) STORE(dest + 2, 57);
+  (void) STORE(dest + 3, 73);
+}
+
+// address: 0100003738.0
+// full-signature: func(_indexed_st4__8_bytes_from_lows, return=[], parameter=[<ptr(int(undef, 1)),dest,reg[x0]>], varargs=false)
+void _indexed_st4__8_bytes_from_lows(d1* dest)
+{
+  (void) STORE(dest + 8, 2820983053732684064);
+  (void) STORE(dest + 16, 3978425819141910832);
+  (void) STORE(dest + 24, 5135868584551137600);
+  (void) STORE(dest, 1663540288323457296);
+}
+
+// address: 010000378c.0
+// full-signature: func(_indexed_st4__8_bytes_from_highs, return=[], parameter=[<ptr(int(undef, 1)),dest,reg[x0]>], varargs=false)
+void _indexed_st4__8_bytes_from_highs(d1* dest)
+{
+  (void) STORE(dest + 8, 3399704436437297448);
+  (void) STORE(dest + 16, 4557147201846524216);
+  (void) STORE(dest + 24, 5714589967255750984);
+  (void) STORE(dest, 2242261671028070680);
+}
+
