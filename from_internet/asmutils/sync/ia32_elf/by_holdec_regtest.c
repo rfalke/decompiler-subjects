736c49a2f7c306d195429ee04bb6bf5a97b1c36a
Raimar Falke <i-git-stone@rf.risimo.net>
2022-01-30 23:09:25 +0100

    Add regression test for new sync subject

---
+++ from_internet/asmutils/sync/ia32_elf/by_holdec_regtest.c
@@ -0,0 +1,11 @@
+// Decompiled by holdec
+
+// address: 0804804c.0
+// full-signature: func(start, return=[], parameter=[], varargs=false)
+void start(void)
+{
+  reg_a = sys_sync();
+  (void) sys_exit(reg_a);
+  // not reached
+}
+
